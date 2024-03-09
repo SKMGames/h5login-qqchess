60288f9bf3a98e794d7a4305b3ed1897428d0246
h2script <113903996+h2script@users.noreply.github.com>
2024-03-09 22:06:10 +0700

    Add files via upload

--- main-2024-03-09.js
+++ main-2024-03-09.js
@@ -1,3 +1,7 @@
+var canvas = document.querySelector("#GameCanvas");
+var gl = canvas.getContext("webgl");
+console.log("gl", gl);
+
 var $jscomp = $jscomp || {};
 $jscomp.scope = {};
 $jscomp.arrayIteratorImpl = function(f) {
